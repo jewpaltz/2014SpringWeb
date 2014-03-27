450820f218e5e8da5899c9511da57bbb99dc6a1d
Moshe Plotkin <mplotkin@jewpaltz.com>
2014-03-27 07:55:53 -0400

    syle

--- Final/Views/Users/edit.php
+++ Final/Views/Users/edit.php
@@ -11,10 +11,18 @@
 	.my-horizontal .form-control{
 		display: inline-block;
 	}
+	.has-feedback .form-control-feedback {
+		display: inline-block;
+		right: auto;
+		top: auto;
+		margin-left: -40px;
+	}
+	.has-error {
+		color: red;
+	}
 	@media screen and (min-width: 768px) {
 		.my-horizontal .form-control{
 			width: 25%;
-			margin-bottom: 5px;
 		}
 	}
 </style>
@@ -34,41 +42,43 @@
 	<input type="hidden" name="id" value="<?=$model['id']?>" />
 	
 	<div class="form-group <?if(isset($errors['FirstName'])) echo 'has-error has-feedback' ?> ">
-		<label for="FirstName">First Name:</label>
+		<label class="control-label" for="FirstName">First Name:</label>
 		<input class="form-control" type="text" name="FirstName" id="FirstName" value="<?=$model['FirstName']?>" placeholder="First Name" />
 		<? if(isset($errors['FirstName'])): ?>
 			<span class="glyphicon glyphicon-remove form-control-feedback"></span>
-			<span><?=$errors['FirstName']?></span>
+			<span ><?=$errors['FirstName']?></span>
 		<? endif ?>
 	</div>
 	
-	
 	<div class="form-group <?if(isset($errors['LastName'])) echo 'has-error has-feedback' ?> ">
-		<label for="LastName">Last Name:</label>
+		<label class="control-label" for="LastName">Last Name:</label>
 		<input class="form-control" type="text" name="LastName" id="LastName" value="<?=$model['LastName']?>" placeholder="Last Name" />
 		<? if(isset($errors['LastName'])): ?>
 			<span class="glyphicon glyphicon-remove form-control-feedback"></span>
-			<span><?=$errors['LastName']?></span>
+			<span ><?=$errors['LastName']?></span>
 		<? endif ?>
 	</div>
 	
-	<label for="Password">Password:</label>
-	<input class="form-control" type="password" name="Password" id="Password" value="<?=$model['Password']?>" placeholder="Password" />
-	<br />
-	<label for="fbid">fbid:</label>
-	<input class="form-control" type="text" name="fbid" id="fbid" value="<?=$model['fbid']?>" placeholder="fbid" />
+	<div class="form-group">
+		<label class="control-label" for="Password">Password:</label>
+		<input class="form-control" type="password" name="Password" id="Password" value="<?=$model['Password']?>" placeholder="Password" />
+	</div>
 	
+	<div class="form-group">
+		<label class="control-label" for="fbid">fbid:</label>
+		<input class="form-control" type="text" name="fbid" id="fbid" value="<?=$model['fbid']?>" placeholder="fbid" />
+	</div>
 
 	<div class="form-group <?if(isset($errors['UserType'])) echo 'has-error has-feedback' ?> ">
-		<label for="UserType">User Type:</label>
+		<label class="control-label" for="UserType">User Type:</label>
 		<input class="form-control" type="text" name="UserType" id="UserType" value="<?=$model['UserType']?>" placeholder="User Type" />
 		<? if(isset($errors['UserType'])): ?>
 			<span class="glyphicon glyphicon-remove form-control-feedback"></span>
-			<span><?=$errors['UserType']?></span>
+			<span ><?=$errors['UserType']?></span>
 		<? endif ?>
 	</div>
 	
-	<label></label>
-	<input class="btn" type="submit" value="Save" />
+	<label class="control-label"></label>
+	<input class="btn btn-primary" type="submit" value="Save" />
 </form>
 
